a4bce8ce255c88af67e90fe930e196ef75137fe4
zhang <mrzhangbin@outlook.com>
2022-06-17 10:28:18 +0800

    start redis cluster in sh

---
+++ scripts/redis.sh
@@ -0,0 +1,8 @@
+redis-server /etc/redis/redis_6487.conf
+redis-server /etc/redis/redis_6486.conf
+redis-server /etc/redis/redis_6485.conf
+redis-server /etc/redis/redis_6484.conf
+redis-server /etc/redis/redis_6483.conf
+redis-server /etc/redis/redis_6482.conf
+redis-server /etc/redis/redis_6481.conf
+
