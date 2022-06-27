8389d5910d922eff3cc662b6b04da0cf19ac74ef
zhang <mrzhangbin@outlook.com>
2022-06-27 13:27:48 +0800

    add kafka send callback

--- src/test/java/com/example/springboot/KafkaTest.java
+++ src/test/java/com/example/springboot/KafkaTest.java
@@ -1,12 +1,14 @@
 package com.example.springboot;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.util.SerializationUtils;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import com.example.springboot.model.Todo;
 
@@ -31,10 +33,16 @@ public class KafkaTest {
     public void testSendTodo() {
         EasyRandom easyRandom = new EasyRandom();
         Todo todo = easyRandom.nextObject(Todo.class);
+        todo.setTitle(RandomStringUtils.randomAlphanumeric(20));
         // kafka.send("topic.todo", "hello kafka");
 
         // kafkaObject.sendDefault(SerializationUtils.serialize(todo));
-        kafkaJson.send("topic.todo.json", todo);
+        ListenableFuture<SendResult<String, Todo>> future = kafkaJson.send("topic.todo.json", todo);
+        future.addCallback(result -> {
+            log.info("send success");
+            log.info(result.getProducerRecord().toString());
+            log.info(result.getRecordMetadata().toString());
+        }, ex -> log.error("send error", ex));
     }
 
 }
